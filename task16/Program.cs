d1bace237d69bfca2aaf6f00fc43731044240e15
Uliana <ashmishmish125@gmail.com>
2022-08-15 16:53:05 +0300

    task16

--- task16/Program.cs
+++ task16/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 16.
+
+
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA / numberB == numberB || numberB / numberA == numberA)
+
+{
+    Console.WriteLine ("является");
+}
+else 
+{
+    Console.WriteLine ("не является");
+}
+
