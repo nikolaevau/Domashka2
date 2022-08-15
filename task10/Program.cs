5815cc6cdc3d93e93ad57738cf448a49c46ff868
Uliana <ashmishmish125@gmail.com>
2022-08-15 17:06:05 +0300

    task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача  10.
+
+Console.WriteLine("Введите трёхзначное число");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString (threeDigitNumber);
+{
+Console.WriteLine ("вторая цифра введённого числа " + stringNumber[1]);
+}
