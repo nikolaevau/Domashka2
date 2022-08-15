d5ce55ce486e2b1eb11ab2252fc460a8962f2518
Uliana <ashmishmish125@gmail.com>
2022-08-15 17:43:20 +0300

    task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13.
+
+Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+string NumberText = Convert.ToString(Number);
+if (NumberText.Length > 2) 
+{
+    Console.WriteLine (NumberText[2]);
+}
+else 
+{
+    Console.WriteLine ("третьей цифры нет");
+}
