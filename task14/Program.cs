b57f7b731ac1f802b99fb4ea36053e0a489481d7
Uliana <ashmishmish125@gmail.com>
2022-08-15 16:36:17 +0300

    task14

--- task14/Program.cs
+++ task14/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 14. Напишите программу, которая принимает на вход число
+
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA%7==0 && numberA%23==0)
+
+{
+Console.WriteLine("да");
+}
+ else 
+ {
+ Console.WriteLine("нет");
+ }
